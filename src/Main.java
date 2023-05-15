5c3624cfd55d84399149219fb4b69af273b8db71
SteinvB <132998970+SteinvB@users.noreply.github.com>
2023-05-15 13:27:25 +0200

    introductie class gemaakt en gebruikt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ik ben Stein.");
-        System.out.println("Ik ben 21 jaar oud.");
-        System.out.println("Ik heb een hond genaamd Bobby");
-        System.out.println("Ik houd van videogames en van lezen");
-        System.out.println("Ik woon in Brabant");
+        introductie tijdelijk = new introductie("stein", "Mijn naam is Duits voor 'steen'.");
     }
 }
---
+++ src/introductie.java
@@ -0,0 +1,7 @@
+public class introductie {
+    public introductie(String naam, String feit0) {
+        System.out.printf("Hoi, ik ben %s!\n", naam);
+        System.out.println("Een feitje over mij:");
+        System.out.println(feit0);
+    }
+}
