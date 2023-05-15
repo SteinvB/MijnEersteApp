631807b95ecd1cba476997cc5a20961bf36f73b8
SteinvB <132998970+SteinvB@users.noreply.github.com>
2023-05-15 12:02:37 +0200

    voltooide(?) opdracht

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Add code
+        System.out.println("Ik ben Stein.");
+        System.out.println("Ik ben 21 jaar oud.");
+        System.out.println("Ik heb een hond genaamd Bobby");
+        System.out.println("Ik houd van videogames en van lezen");
+        System.out.println("Ik woon in Brabant");
     }
 }
